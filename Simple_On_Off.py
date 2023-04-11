fa1f9cf4500c979944e6812be4bd0af57e8ae171
Alan Bell <alanebell@mac.com>
2023-04-11 17:04:30 -0400

    Chat gpt 4 version

--- Simple_On_Off.py
+++ Simple_On_Off.py
@@ -1,38 +1,41 @@
 import paho.mqtt.client as mqtt
 import time
 
-# Enter the IP address of your Smart Plug 2
-broker_address = "xxx.xxx.xxx.xxx"
+# Replace with the appropriate values for your MQTT broker and smart plug
+MQTT_BROKER = "your_mqtt_broker_address"
+MQTT_PORT = 1883
+MQTT_TOPIC = "your_smart_plug_topic"
+MQTT_USERNAME = "your_username"
+MQTT_PASSWORD = "your_password"
 
-# Enter the MQTT topics for controlling the Smart Plug 2
-topic_on = "smartplug2/on"
-topic_off = "smartplug2/off"
 
-# Enter your MQTT username and password (if applicable)
-username = ""
-password = ""
-
-# Define the MQTT callback functions
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    print(f"Connected with result code {rc}")
 
-def on_publish(client, userdata, mid):
-    print("Message published")
 
-# Connect to the MQTT broker
+def on_message(client, userdata, msg):
+    print(f"{msg.topic}: {msg.payload.decode()}")
+
+
 client = mqtt.Client()
-client.username_pw_set(username, password)
 client.on_connect = on_connect
-client.on_publish = on_publish
-client.connect(broker_address, 1883, 60)
+client.on_message = on_message
 
-# Turn the Smart Plug 2 on
-client.publish(topic_on, payload="on", qos=0, retain=False)
-time.sleep(1)
+client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+client.connect(MQTT_BROKER, MQTT_PORT, 60)
 
-# Turn the Smart Plug 2 off
-client.publish(topic_off, payload="off", qos=0, retain=False)
-time.sleep(1)
+client.loop_start()
 
-# Disconnect from the MQTT broker
+# Turn the smart plug on
+print("Turning the smart plug on")
+client.publish(MQTT_TOPIC, "ON")
+time.sleep(5)
+
+# Turn the smart plug off
+print("Turning the smart plug off")
+client.publish(MQTT_TOPIC, "OFF")
+
+time.sleep(5)
+
+client.loop_stop()
 client.disconnect()
