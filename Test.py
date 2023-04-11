dc8de321ab78b9668293fe66340f38672af09058
Harlan Bell <hrlnbell@gmail.com>
2023-04-11 19:22:40 -0400

    added program to calculate the force of gravity between two objects

--- Test.py
+++ Test.py
@@ -1,3 +1,10 @@
 print("Hello World")
 test = "hello world"
 print(test)
+import math
+g = 6.67e-11
+mass1 = float(input("Mass of the first object (kg):"))
+mass2 = float(input("Mass of the second object (kg):"))
+r = float(input("Distance between objects (meters):"))
+fg = (g*mass1*mass2)/(r*r)
+print("Force in newtons: ", fg)
